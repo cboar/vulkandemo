6096d104ba20aee3f725dd4b5bff7517bfdacc83
cboar <admin@cboar.org>
2017-09-30 07:16:11 -0700

    Separated Vulkan init functions, added Logical device and Graphics queue creation.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -13,23 +14,10 @@ void init_window(GLFWwindow** window){
 	*window = glfwCreateWindow(800, 600, "Photon", NULL, NULL);
 }
 
-bool is_device_valid(VkPhysicalDevice* device){
+bool create_instance(VkInstance* instance){
 	uint32_t count;
-	VkQueueFamilyProperties queues[32];
-	vkGetPhysicalDeviceQueueFamilyProperties(*device, &count, NULL);
-	vkGetPhysicalDeviceQueueFamilyProperties(*device, &count, queues);
-	for(uint32_t i = 0; i < count; i++){
-		if(queues[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
-			return true;
-	}
-	return false;
-}
-
-void init_vulkan(VkInstance* instance, VkPhysicalDevice* device){
-	uint32_t count;
-
-	/* VULKAN INSTANCE */
-	VkApplicationInfo app_info = {
+	VkApplicationInfo app_info = {0};
+	app_info = (VkApplicationInfo){
 		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
 		.pApplicationName = "Photon",
 		.applicationVersion = VK_MAKE_VERSION(1, 0, 0),
@@ -37,31 +25,72 @@ void init_vulkan(VkInstance* instance, VkPhysicalDevice* device){
 		.engineVersion = VK_MAKE_VERSION(1, 0, 0),
 		.apiVersion = VK_API_VERSION_1_0,
 	};
-	VkInstanceCreateInfo create_info = {
+	VkInstanceCreateInfo create_info = {0};
+	create_info = (VkInstanceCreateInfo){
 		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
 		.pApplicationInfo = &app_info,
 		.enabledLayerCount = 0,
 	};
 	create_info.ppEnabledExtensionNames = glfwGetRequiredInstanceExtensions(&count);
 	create_info.enabledExtensionCount = count;
-	if(vkCreateInstance(&create_info, NULL, instance) != VK_SUCCESS){
-		fprintf(stderr, "Failed to create instance.\n");
-	}
+	return vkCreateInstance(&create_info, NULL, instance) == VK_SUCCESS;
+}
 
-	/* VULKAN DEVICE */
-	*device = VK_NULL_HANDLE;
-	VkPhysicalDevice devices[32];
-	vkEnumeratePhysicalDevices(*instance, &count, NULL);
-	vkEnumeratePhysicalDevices(*instance, &count, devices);
+uint32_t graphics_queue_index(VkPhysicalDevice* device){
+	uint32_t count;
+	VkQueueFamilyProperties queues[32];
+	vkGetPhysicalDeviceQueueFamilyProperties(*device, &count, NULL);
+	vkGetPhysicalDeviceQueueFamilyProperties(*device, &count, queues);
 	for(uint32_t i = 0; i < count; i++){
-		if(is_device_valid(devices + i)){
-			*device = devices[i];
-			break;
+		if(queues[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+			return i;
+	}
+	return -1;
+}
+
+bool pick_phys_device(VkPhysicalDevice* phys_device, uint32_t* queue_index, VkInstance instance){
+	uint32_t count;
+	VkPhysicalDevice devices[32];
+	vkEnumeratePhysicalDevices(instance, &count, NULL);
+	vkEnumeratePhysicalDevices(instance, &count, devices);
+	for(uint32_t i = 0; i < count; i++){
+		uint32_t temp = graphics_queue_index(devices + i);
+		if(temp != -1){
+			*phys_device = devices[i];
+			*queue_index = temp;
+			return true;
 		}
 	}
-	if(*device == VK_NULL_HANDLE){
-		fprintf(stderr, "Failed to find valid physical device.\n");
-	}
+	return false;
+}
+
+bool create_logical_device(VkDevice* logical_device, VkPhysicalDevice phys_device, uint32_t queue_index){
+	float priority = 1.0f;
+	VkDeviceQueueCreateInfo queue_create_info = { 0 };
+	queue_create_info = (VkDeviceQueueCreateInfo){
+		.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+		.queueFamilyIndex = queue_index,
+		.pQueuePriorities = &priority,
+		.queueCount = 1,
+	};
+	VkPhysicalDeviceFeatures features = { 0 };
+	VkDeviceCreateInfo dev_create_info = { 0 };
+	dev_create_info = (VkDeviceCreateInfo){
+		.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+		.pQueueCreateInfos = &queue_create_info,
+		.pEnabledFeatures = &features,
+		.queueCreateInfoCount = 1,
+		.enabledLayerCount = 0,
+	};
+	return vkCreateDevice(phys_device, &dev_create_info, NULL, logical_device) == VK_SUCCESS;
+}
+
+void init_vulkan(VkInstance* instance, VkPhysicalDevice* phys_device, VkDevice* logical_device, VkQueue* graphics_queue){
+	uint32_t queue_index;
+	create_instance(instance);
+	pick_phys_device(phys_device, &queue_index, *instance);
+	create_logical_device(logical_device, *phys_device, queue_index);
+	vkGetDeviceQueue(*logical_device, queue_index, 0, graphics_queue);
 }
 
 int main(void){
@@ -69,14 +98,17 @@ int main(void){
 	init_window(&window);
 
 	VkInstance instance;
-	VkPhysicalDevice device;
-	init_vulkan(&instance, &device);
+	VkPhysicalDevice phys_device;
+	VkDevice logical_device;
+	VkQueue graphics_queue;
+	init_vulkan(&instance, &phys_device, &logical_device, &graphics_queue);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 	}
 
 	vkDestroyInstance(instance, NULL);
+	vkDestroyDevice(logical_device, NULL);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 }
